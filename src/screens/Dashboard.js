19eab2c8bf393cbb9008ab1f6eb47d7fc41dc82d
Martim Silva <martimtsilva@hotmail.com>
2022-11-24 12:19:32 +0000

    Get User details from firebase

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,8 +1,27 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import { db } from "../../firebase";
 
 export default function Dashboard({ navigation }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    try {
+      AsyncStorage.getItem("@userData").then((data) => {
+        db.collection("users")
+          .doc(JSON.parse(data).id)
+          .get()
+          .then((snapshot) => {
+            if (snapshot.exists) {
+              setUser(snapshot.data());
+            }
+          });
+      });
+    } catch (error) {}
+  }, []);
+
   const onLogout = () => {
     AsyncStorage.removeItem("@userData");
     navigation.replace("Login");
@@ -10,7 +29,7 @@ export default function Dashboard({ navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text>Hello !</Text>
+      <Text>Hello {user?.name}!</Text>
       <Button onPress={onLogout} title="Logout"></Button>
       <StatusBar style="auto" />
     </View>
